e3991061488debfeedc44993fe75c4bd6e3d965b
Bruno Magalhaes <bruno_magalhaes_oli@hotmail.com>
2022-07-13 12:31:27 -0300

    .env added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRouter = require('./api/users/userRouter')
@@ -6,6 +7,6 @@ app.use(express.json())
 
 app.use('/api/users', userRouter)
 const port = 3000
-app.listen(port, () => {
-  console.log('Dale server rodando na porta: ', port)
+app.listen(process.env.APP_PORT, () => {
+  console.log('Dale server rodando na porta: ', process.env.APP_PORT)
 })
